8f9e09156f103c7c671a98e9433afb96a1d14749
Prashant tyagi <88424635+Tyagiweb@users.noreply.github.com>
2023-08-28 23:29:47 +0530

    Add files via upload

--- projectnew/app1/urls.py
+++ projectnew/app1/urls.py
@@ -4,6 +4,11 @@ from app1 import views
 
 urlpatterns = [
     path('', views.register,name='register'),
+    path('login/', views.login,name='login'),
+    path('registerform/', views.registerform,name='registerform'),
     path('data/', views.data,name='data'),
-    path('data/viewdata/', views.viewdata,name='viewdata'),
+    path('viewdata/', views.viewdata,name='viewdata'),
+    path('login/validuser', views.validuser,name='validuser'),
+    
+
 ]
--- projectnew/app1/views.py
+++ projectnew/app1/views.py
@@ -3,6 +3,12 @@ from .models import Student
 
 # Create your views here.
 def register(request):
+    return render(request,'home.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def registerform(request):
     return render(request,'register.html')
 
 
@@ -21,4 +27,33 @@ def data(request):
 def viewdata(request):
     all_data=Student.objects.all()
     return render(request,'submit.html',{'key1':all_data})
+
+
+    
+          
+    
+
+def validuser(request):
+    if request.method=="POST":
+        firstname=request.POST['username']
+        password=request.POST['password']
+
+        user=Student.objects.get(Firstname=firstname)
+        if user:
+            if user.Psw==password:
+                request.session['Name']=user.Firstname
+                request.session['Email']=user.Email
+                return render(request,'Afterlogin.html')
+            else:
+                message="Password does not match"
+                return render(request,'login.html',{'msg':message})
+        else:
+            message="User not exist"
+            return render(request,'register.html',{'msg':message})    
+    
+      
+
+
+
+
 
